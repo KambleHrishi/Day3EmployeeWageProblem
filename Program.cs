ab63a8a610834b02f99d81caaad8b001e04583dc
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-06 22:35:32 +0530

    UC2 Daily Wage of employee

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("UC1 checking if employee is present or absent");
-            UC1_Attendance.Attendance();
+            //Console.WriteLine("UC1 checking if employee is present or absent");
+            //UC1_Attendance.Attendance();
+            Console.WriteLine("UC2: Calculate Daily Employee Wage");
+            UC2_DailyWage.DailyWage();
         }
     }
 }
---
+++ UC2_DailyWage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3EmployeeWageProblem
+{
+    internal class UC2_DailyWage
+    {
+        public static void DailyWage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();// new object creation
+            int empCheck = random.Next(0, 2);//0 is inclusive,2 is exclusive: 0,1
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
