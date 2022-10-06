496f1f1f00b3c621559e18bfecc8ce3c0f6b2fd9
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-06 22:46:18 +0530

    added part time employee and wage

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@
         {
             //Console.WriteLine("UC1 checking if employee is present or absent");
             //UC1_Attendance.Attendance();
-            Console.WriteLine("UC2: Calculate Daily Employee Wage");
-            UC2_DailyWage.DailyWage();
+            //Console.WriteLine("UC2: Calculate Daily Employee Wage");
+            //UC2_DailyWage.DailyWage();
+            Console.WriteLine("UC3: adding part time employee and wage");
+            UC3_PartTime.PartTime();
         }
     }
 }
---
+++ UC3_PartTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3EmployeeWageProblem
+{
+    internal class UC3_PartTime
+    { 
+        public static void PartTime()
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();// new object creation
+            int empCheck = random.Next(0, 3);//0 is inclusive,3 is exclusive: 0,1,2
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+       
+    }
+}
